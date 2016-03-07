a5e4d56b2b6944d71e8444d3969a5aaaf869c05d
Haythem Hammour <hammour@gmail.com>
2016-03-07 14:25:05 -0600

    Mid Lab

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var noun = prompt("Enter a Noun");
+var verb = prompt("Enter a Verb");
+var adj = prompt("Enter an Adjective");
+var adv = prompt("Enter an Adverb");
+
+console.log("Do you "+verb+ " your "+adj+" "+noun+" "+adv+"? That's hilarious!");
